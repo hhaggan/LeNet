87d8ec3f93a9a4cccd68918ac6c4d3b8d6c70434
hhaggan <hhaggan@hotmail.com>
2019-05-21 23:13:02 -0400

    Initializing the high level approach for the LeNet Architecture
    Initializing the high level approach

---
+++ LeNet.py
@@ -0,0 +1,35 @@
+import numpy as np
+import tensorflow as tf
+import math
+import os
+import random 
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+#tensorflow
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.layers import flatten
+
+#Assigning the input data to the test/validation/train variable
+
+#defining the tensorflow hyperparameters
+
+#function to train LeNet
+
+
+def LeNet(x):
+    '''this function has 2 convolution networks with the activation functions and Pooling
+    3 fully connected layers with their activation functions'''
+
+
+    #First convolution
+    '''The Input for the images should be 28*28*6 for the first convolution layer'''
+
+    '''The Pooling for the first convolution layer should be 14*14*6'''
+
+    #Second Convolution
+    '''The Input for the images should be 28*28*6 for the first convolution layer'''
+
+    '''The Pooling for the first convolution layer should be 14*14*6'''
+
+#function to evaluate
+def evaluate():
