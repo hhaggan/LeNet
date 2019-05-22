5845596275d3ae76e46dbd3d0cf9f32cb9b0c420
hhaggan <hhaggan@hotmail.com>
2019-05-22 14:35:02 -0400

    New Updates
    Pushing the new updates... still need a lot of work

--- LeNet.py
+++ LeNet.py
@@ -10,26 +10,102 @@ from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.contrib.layers import flatten
 
 #Assigning the input data to the test/validation/train variable
+mnist = input_data.read_data_sets("MNIST_data/", reshape=False)
+X_train, y_train           = mnist.train.images, mnist.train.labels
+X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+X_test, y_test             = mnist.test.images, mnist.test.labels
 
 #defining the tensorflow hyperparameters
+EPOCHS = 10
+BATCH_SIZE = 128
+rate = 0.001
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 10)
+
+#Defining the weights and the biases that will be used in the Network
+# Store layers weight & bias
+weights = {
+    'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+    'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+    'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([32])),
+    'bc2': tf.Variable(tf.random_normal([64])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))}
 
 #function to train LeNet
-
-
 def LeNet(x):
     '''this function has 2 convolution networks with the activation functions and Pooling
     3 fully connected layers with their activation functions'''
-
+    mu = 0
+    sigma = 0.1
 
     #First convolution
     '''The Input for the images should be 28*28*6 for the first convolution layer'''
+    conv1_w = tf.Variable(tf.truncated_normal(shape=(5,5,1,6), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(x, conv1_w, strides=[1,1,1,1], padding='VALID') + conv1_b
 
+    #Activation function
+    conv1 = tf.nn.relu(conv1)
     '''The Pooling for the first convolution layer should be 14*14*6'''
-
+    conv1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+     
     #Second Convolution
-    '''The Input for the images should be 28*28*6 for the first convolution layer'''
+    '''The Input for the images should be 10*10*16 for the first convolution layer'''
+    conv_w = tf.Variable(tf.truncated_normal(shape=(5,5,6,16), mean=mu, stddev=sigma))
+    conv_b = tf.Variable(tf.zeros(6))
+    conv2 = tf.nn.conv2d(conv1, conv2_w, strides=[1,1,1,1], padding='VALID') + conv_b
 
-    '''The Pooling for the first convolution layer should be 14*14*6'''
+    #Activation Function
+    conv2 = tf.nn.relu(conv2)
+    '''The Pooling for the first convolution layer should be 5*5*16'''
+    conv2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+
+    #Flatten the Output 
+    '''This to flatten the output to 1D rather than 3D'''
+    fc0 = flatten(conv2)
+
+    #First Fully Connected Layer
+    '''The Input for the images should be 28*28*6 for the first convolution layer'''
+    fc1_w = tf.Variable(tf.truncated_normal(shape =(400,120)), mean=mu, stddev=sigma)
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc0, fc1_w) + fc1_b
+
+    '''The output should be 120'''
+    fc1 = tf.nn.relu(fc1)
+
+    #Second Fully Connected Layer
+    '''The Input for the images should be 28*28*6 for the first convolution layer'''
+    fc2_w = tf.Variable(tf.truncated_normal(shape=(120, 84)), mean=mu, stddev=sigma)
+    fc2_b = tf.Variable(tf.zeros(84))
+    fc2 = tf.matmul(fc1, fc2_w) + fc2_b
+
+    '''The output should be 84'''
+    fc2 = tf.nn.relu(fc2)
+
+    #Third Fully Connected Layer
+    '''The Input for the images should be 28*28*6 for the first convolution layer'''
+    fc3_w = tf.Variable(tf.truncated_normal(shape=(84,10)), mean=mu, stddev=sigma)
+    fc3_b = tf.Variable(tf.zeros(10))
+    logits = tf.matmul(fc2, fc3_w) + fc3_b
+    '''The Final output should be 10 as per the MNIST data'''
+    return logits
 
 #function to evaluate
 def evaluate():
+
+#Deep Learning details
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_function = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=rate)
+training = optimizer.minimize(loss_function)
+
+#Train The Model
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
